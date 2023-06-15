2ea1cdafa2ebd79774fcf27e91bec0761ce6fd3d
cc30f <78901500+miroutom@users.noreply.github.com>
2023-06-16 01:27:47 +0300

    Add files via upload

--- toDo.py
+++ toDo.py
@@ -2,6 +2,15 @@ import argparse
 
 
 def add_task(tasks, title, description) -> None:
+    """
+    Add a new todo item
+
+    :param tasks: required tasks to do
+    :param title: title of todo item
+    :param description: description of todo item
+    :return:
+    """
+
     task = {
         "title": title,
         "description": description,
@@ -12,6 +21,12 @@ def add_task(tasks, title, description) -> None:
 
 
 def view_tasks(tasks) -> None:
+    """
+    View all the tasks
+    :param tasks: existed tasks
+    :return:
+    """
+
     print("List of tasks:")
     for index, task in enumerate(tasks):
         print(f"{index + 1}. Title: {task['title']}")
@@ -20,6 +35,13 @@ def view_tasks(tasks) -> None:
 
 
 def complete_task(tasks, title) -> None:
+    """
+    Mart task as completed
+    :param tasks: existed tasks
+    :param title: title of task to mark as completed
+    :return:
+    """
+
     for task in tasks:
         if task['title'] == title:
             task['completed'] = True
@@ -29,6 +51,12 @@ def complete_task(tasks, title) -> None:
 
 
 def delete_task(tasks, title) -> None:
+    """
+    Delete a todo item
+    :param tasks: existed task
+    :param title: title of task to delete
+    :return:
+    """
     for task in tasks:
         if task['title'] == title:
             del tasks[task]
@@ -38,12 +66,25 @@ def delete_task(tasks, title) -> None:
 
 
 def save_tasks(tasks, filename):
+    """
+    Save all the tasks to file
+    :param tasks: existed tasks
+    :param filename: file to write info
+    :return:
+    """
+
     with open(filename, 'w') as file:
         for task in tasks:
             file.write(f"{task['title']},{task['description']},{task['completed']}\n")
 
 
 def load_tasks(filename):
+    """
+    Load all the tasks from file
+    :param filename: file to upload
+    :return:
+    """
+
     tasks = []
     with open(filename, 'r') as file:
         for line in file:
@@ -58,6 +99,11 @@ def load_tasks(filename):
 
 
 def main() -> None:
+    """
+    Main function
+    :return:
+    """
+
     parser = argparse.ArgumentParser(description="ToDo List Manager")
     parser.add_argument('--file', default='todo_list.txt', help='Path to file')
     parser.add_argument('command', choices=['add', 'complete', 'delete', 'view'])
